4736b4ab4f62f41f980ce16e3f16208e2bc01c2c
supssk <140276810+supssk@users.noreply.github.com>
2023-07-31 01:49:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.color = "#fff";
+    skills.style.color = "#000";
+    projects.style.color = "#000";
+    contact.style.color = "#000";
+    about.style.color = "#000";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+
+}
